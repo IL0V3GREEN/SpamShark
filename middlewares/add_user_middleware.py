145eb2bb3da65b1a8bfa5b7e8c21c74913bc58a7
SSH <97981661+gojuka1@users.noreply.github.com>
2023-07-30 21:04:41 +0300

    Add files via upload

---
+++ middlewares/add_user_middleware.py
@@ -0,0 +1,23 @@
+from typing import Callable, Dict, Any, Awaitable
+from aiogram import BaseMiddleware
+from aiogram.types import Message
+from mongo import Database
+
+
+db = Database()
+
+
+class CounterMiddleware(BaseMiddleware):
+    def __init__(self) -> None:
+        self.counter = 0
+
+    async def __call__(
+        self,
+        handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+        event: Message,
+        data: Dict[str, Any]
+    ) -> Any:
+        if not db.user_info(event.from_user.id):
+            db.add_user(event.from_user.id)
+
+        return await handler(event, data)
