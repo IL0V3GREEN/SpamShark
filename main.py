e3d5b6853ebccdcbf60c6256bad402d2ddf72fe7
SSH <97981661+gojuka1@users.noreply.github.com>
2023-08-04 00:49:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from aiocryptopay import AioCryptoPay, Networks
 from handlers import start_handler, spamCreate_handlers, parsing_handlers, balance_handlers
 from middlewares.add_user_middleware import CounterMiddleware
 import logging
@@ -8,8 +7,6 @@ from aiohttp import web
 from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 
-
-crypto = AioCryptoPay("8055:AAXZx66BTYEP7WC2k0Wl9JpR8dmPUF8WOuN", Networks.TEST_NET)
 
 BASE_URL = "https://spamshark-production.up.railway.app"
 MAIN_BOT_TOKEN = "6249367873:AAFra-Kvtu6i1V9lS8kvx_9J8-XGxDTxCI8"
@@ -38,7 +35,7 @@ def main():
     dp.startup.register(on_startup)
 
     app = web.Application()
-    app.add_routes([web.post("/8055:AAXZx66BTYEP7WC2k0Wl9JpR8dmPUF8WOuN", crypto.get_updates)])
+    app.router.add_post("/8055:AAXZx66BTYEP7WC2k0Wl9JpR8dmPUF8WOuN", hello)
     SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path=MAIN_BOT_PATH)
     setup_application(app, dp, bot=bot)
     web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
--- post.py
+++ post.py
@@ -1,20 +1,14 @@
 from aiohttp import web
 from mongo import Database
 import requests
-from aiocryptopay import AioCryptoPay, Networks
-from aiocryptopay.models.update import Update
-
 
 mongo = Database()
-crypto = AioCryptoPay("8055:AAXZx66BTYEP7WC2k0Wl9JpR8dmPUF8WOuN", Networks.TEST_NET)
 
 
-@crypto.pay_handler()
-async def hello(update: Update, app):
-    text = "Hello!!! 200!!! EVERYTHING OK!!!"
-    print(update)
+async def hello(request):
+    print(request)
     try:
-        return web.Response(text=text)
+        return web.Response(text="Hello!!! 200!!! EVERYTHING OK!!!")
 
     except KeyError:
         return web.Response(text="SOME PARAMS MISSING!!!")
