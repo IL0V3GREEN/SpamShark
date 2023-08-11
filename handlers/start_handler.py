3666163a6616d30af17e8e18bfd543f7b8e066e9
Maximus <97981661+gojuka1@users.noreply.github.com>
2023-08-12 02:55:49 +0300

    Add files via upload

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,10 +1,10 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
-from aiogram.types import ReplyKeyboardRemove
 from aiogram.fsm.context import FSMContext
 from keyboards.spamCreator_buttons import choose_theme
 from mongo import Database
+from keyboards.support_button import support_link
 
 
 db = Database()
@@ -15,15 +15,12 @@ router = Router()
 async def start_handle(message: Message, state: FSMContext):
     if message.text[7:] == "":
         await message.answer(
-            "Приветствуем тебя в SpamShark!\n\n"
+            "<b>Приветствуем тебя в SpamShark!</b>\n\n"
             "👨🏻‍🏫 Наш сервис поможет тебе спарсить участников открытых чатов, а также, ты "
             "можешь воспользоваться нашим платным сервисом быстрой "
-            "<a href='https://t.me/spamsharkbot?start=spamcreate'>спам-рассылки</a> 📩\n\n"
-            'Новостной канал: <b>@spamshark</b>\n'
-            "Поддержка: <b>@rrassvetov</b>\n\n"
-            '<i>Чтобы начать пользоваться ботом, нажми кнопку "Меню"</i>',
-            reply_markup=ReplyKeyboardRemove(),
-            disable_web_page_preview=True
+            "<a href='https://t.me/spamsharkbot?start=spamcreate'><b>спам-рассылки</b></a> 📩\n\n"
+            '<i>Чтобы начать пользоваться ботом, нажми кнопку <b>"Меню"</b></i>',
+            reply_markup=support_link()
         )
     elif message.text[7:] == "spamcreate":
         await message.answer(
