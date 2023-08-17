36b5bbd360024d25fcae3dc8c3ffb6dda501c65e
Maximus <97981661+gojuka1@users.noreply.github.com>
2023-08-17 23:36:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from handlers import start_handler, spamCreate_handlers, parsing_handlers, profile_handlers, info_handlers
 import logging
+from middleware import deleting_message
 from aiogram import Bot, Dispatcher
 from aiohttp import web
 from aiogram.client.session.aiohttp import AiohttpSession
@@ -25,6 +26,7 @@ def main():
     bot_settings = {"session": session, "parse_mode": "HTML"}
     bot = Bot(token=MAIN_BOT_TOKEN, **bot_settings)
     dp = Dispatcher()
+    dp.message.middleware.register(deleting_message.DeletingMessage)
     dp.include_router(start_handler.router)
     dp.include_router(spamCreate_handlers.router)
     dp.include_router(parsing_handlers.router)
---
+++ middleware/deleting_message.py
@@ -0,0 +1,17 @@
+from typing import Callable, Dict, Any, Awaitable
+from aiogram import BaseMiddleware
+from aiogram.types import Message
+
+
+class DeletingMessage(BaseMiddleware):
+    def __init__(self) -> None:
+        pass
+
+    async def __call__(
+            self,
+            handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+            event: Message,
+            data: Dict[str, Any]
+    ) -> Any:
+        await event.delete()
+        return await handler(event, data)
